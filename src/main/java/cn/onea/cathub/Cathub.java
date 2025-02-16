e85a97bad52912c9d6c5bf8a3bb791be091a2e18
oneachina <ruiruixi@hotmail.com>
2025-02-16 17:51:33 +0800

    cathub

---
+++ src/main/java/cn/onea/cathub/Cathub.java
@@ -0,0 +1,17 @@
+package cn.onea.cathub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cathub extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
