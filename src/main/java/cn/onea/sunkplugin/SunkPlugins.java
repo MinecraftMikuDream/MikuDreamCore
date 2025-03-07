f8eee861348f80740d54b687a373f14c51bed503
oneachina <ruiruixi@hotmail.com>
2025-03-07 22:25:05 +0800

    dupe check

--- src/main/java/cn/onea/sunkplugin/SunkPlugins.java
+++ src/main/java/cn/onea/sunkplugin/SunkPlugins.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,9 +15,22 @@ import org.jetbrains.annotations.NotNull;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
-import java.net.http.HttpClient;
-import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Trident;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class SunkPlugins extends JavaPlugin implements Listener {
@@ -26,6 +40,7 @@ public class SunkPlugins extends JavaPlugin implements Listener {
     private boolean featureEnabled = true;
     private static final String ADMIN_PERMISSION = "cat.admin";
     private static final String CONFIG_KEY = "kill_enabled";
+    private final Set<UUID> readyThrow = new HashSet<UUID>();
 
     @Override
     public void onEnable() {
@@ -221,5 +236,44 @@ public class SunkPlugins extends JavaPlugin implements Listener {
         }
     }
 
-
+//    @EventHandler
+//    public void onProjectileLaunch(ProjectileLaunchEvent event) {
+//        if (event.getEntity() instanceof Trident && event.getEntity().getShooter() instanceof Player) {
+//            Player player = (Player)((Object)event.getEntity().getShooter());
+//            this.readyThrow.remove(player.getUniqueId());
+//        }
+//    }
+//
+//    @EventHandler
+//    public void onPlayerInteract(PlayerInteractEvent event) {
+//        ItemStack item = event.getItem();
+//        if (item == null) {
+//            return;
+//        }
+//        if (!(event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK || item.getType() != Material.TRIDENT || item.containsEnchantment(Enchantment.RIPTIDE))) {
+//            this.readyThrow.add(event.getPlayer().getUniqueId());
+//        }
+//    }
+//
+//    @EventHandler(priority=EventPriority.LOWEST)
+//    public void onInventoryClick(InventoryClickEvent event) {
+//        HumanEntity humanEntity = event.getWhoClicked();
+//        if (this.readyThrow.contains(humanEntity.getUniqueId())) {
+//            event.setCancelled(true);
+//        }
+//    }
+//
+//    @EventHandler
+//    public void onPlayerItemHeld(PlayerItemHeldEvent event) {
+//        Player player = event.getPlayer();
+//        ItemStack itemStack = player.getInventory().getItem(event.getNewSlot());
+//        if (itemStack == null || itemStack.getType() != Material.TRIDENT) {
+//            this.readyThrow.remove(player.getUniqueId());
+//        }
+//    }
+//
+//    @EventHandler
+//    public void onPlayerQuit(PlayerQuitEvent event) {
+//        this.readyThrow.remove(event.getPlayer().getUniqueId());
+//    }
 }
---
+++ src/main/java/cn/onea/sunkplugin/anitcheat/impl/DupeCheck/DupeCheck.java
@@ -0,0 +1,64 @@
+package cn.onea.sunkplugin.anitcheat.impl.DupeCheck;
+
+import org.bukkit.event.Listener;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Trident;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class DupeCheck implements Listener {
+    private final Set<UUID> readyThrow = new HashSet<UUID>();
+    @EventHandler
+    public void onProjectileLaunch(ProjectileLaunchEvent event) {
+        if (event.getEntity() instanceof Trident && event.getEntity().getShooter() instanceof Player) {
+            Player player = (Player)((Object)event.getEntity().getShooter());
+            this.readyThrow.remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        ItemStack item = event.getItem();
+        if (item == null) {
+            return;
+        }
+        if (!(event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK || item.getType() != Material.TRIDENT || item.containsEnchantment(Enchantment.RIPTIDE))) {
+            this.readyThrow.add(event.getPlayer().getUniqueId());
+        }
+    }
+
+    @EventHandler(priority=EventPriority.LOWEST)
+    public void onInventoryClick(InventoryClickEvent event) {
+        HumanEntity humanEntity = event.getWhoClicked();
+        if (this.readyThrow.contains(humanEntity.getUniqueId())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerItemHeld(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        ItemStack itemStack = player.getInventory().getItem(event.getNewSlot());
+        if (itemStack == null || itemStack.getType() != Material.TRIDENT) {
+            this.readyThrow.remove(player.getUniqueId());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        this.readyThrow.remove(event.getPlayer().getUniqueId());
+    }
+}
